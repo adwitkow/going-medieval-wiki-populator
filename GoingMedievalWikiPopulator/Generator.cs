910fd20aaa8c35750dc1683ff14140838eb18bd8
adwitkow <adamadik@poczta.fm>
2023-07-08 12:53:37 +0200

    Use SubGenerators automatically

---
+++ GoingMedievalWikiPopulator/Generator.cs
@@ -0,0 +1,24 @@
+﻿namespace GoingMedievalWikiPopulator
+{
+    internal abstract class Generator : IGenerator
+    {
+        public abstract string Directory { get; }
+
+        protected abstract IEnumerable<ISubGenerator> SubGenerators { get; }
+
+        public GenerationResult[] Generate()
+        {
+            var results = new List<GenerationResult>();
+
+            if (SubGenerators is not null)
+            {
+                foreach (var subGenerator in SubGenerators)
+                {
+                    results.AddRange(subGenerator.Generate());
+                }
+            }
+
+            return results.ToArray();
+        }
+    }
+}
--- GoingMedievalWikiPopulator/Generators/Resources/ResourcesGenerator.cs
+++ GoingMedievalWikiPopulator/Generators/Resources/ResourcesGenerator.cs
@@ -1,51 +1,16 @@
-﻿using GoingMedievalWikiPopulator.JsonModels.Resources;
-
-namespace GoingMedievalWikiPopulator.Generators.Resources
+﻿namespace GoingMedievalWikiPopulator.Generators.Resources
 {
-    internal class ResourcesGenerator : IGenerator
+    internal class ResourcesGenerator : Generator
     {
-        private readonly Dictionary<string, Resource> _resources;
-        private readonly LocalizationProvider _localizationProvider;
-        private readonly DecayGenerator _decayGenerator;
+        private readonly IEnumerable<ISubGenerator> _subGenerators;
 
-        public string Directory => "Resources";
-
-        public ResourcesGenerator(LocalizationProvider locProvider, GameModelProvider modelProvider, DecayGenerator decayGenerator)
+        public ResourcesGenerator(DecayGenerator decayGenerator)
         {
-            _localizationProvider = locProvider;
-            _decayGenerator = decayGenerator;
-
-            var resourceModel = modelProvider.GetModel<ResourceModel>();
-            _resources = ProcessResources(resourceModel);
+            _subGenerators = new[] { decayGenerator };
         }
 
-        public GenerationResult[] Generate()
-        {
-            var results = new List<GenerationResult>();
+        public override string Directory => "Resources";
 
-            foreach (var resource in _resources.Values)
-            {
-                var name = _localizationProvider.Localize(resource.LocKeys[0].Name).Trim();
-                var path = Path.Combine(name, "test");
-                var result = new GenerationResult(path, Array.Empty<string>());
-                results.Add(result);
-            }
-
-            results.AddRange(_decayGenerator.Generate());
-
-            return results.ToArray();
-        }
-
-        private static Dictionary<string, Resource> ProcessResources(ResourceModel resourceModel)
-        {
-            var results = new Dictionary<string, Resource>();
-
-            foreach (var resource in resourceModel.Resources)
-            {
-                results.Add(resource.Id, resource);
-            }
-
-            return results;
-        }
+        protected override IEnumerable<ISubGenerator> SubGenerators => _subGenerators;
     }
 }
